4ec582a525062cfbcca752d8adf70aa27c8fe19b
SADHNA <SADHNA@LAPTOP-77TH1IL2>
2023-06-15 17:12:40 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudTask.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+           
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudTask.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
